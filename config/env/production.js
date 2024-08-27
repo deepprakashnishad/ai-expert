3ad50b3bb80b13fe450811804b663d1649e6bb9c
Giriraj Store <giriraj-store@gmail.com>
2024-08-27 16:19:29 +0530

    Added Shopify details to production

--- config/env/production.js
+++ config/env/production.js
@@ -422,6 +422,18 @@ module.exports = {
       username: 'vipin@astratechsystems.com',
       password: 't0cCmj7x'
     },
+    
+    SHOPIFY: {
+      shop_name: "6cdc10-40",
+
+      admin_token: "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",
+
+      store_token: "81a53db4b8b9d2840d8c445c486a9bdb",
+
+      api_key: "33a12bf566722a928ad9e777d51ac463",
+
+      api_secret_key: "d7b3305c15a02b90f9b6fbac2122b7b6"
+    },
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
     // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
